1c49f5bbc83bf5eafb0a493420e0e09b60680419
Vigoraj_bty <bvigoraj@gmail.com>
2025-08-16 23:13:19 +0530

    initial commit

---
+++ LUCKY_N/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+// import './App.css'
+import Die from './Die'
+import Dice from './Dice'
+
+function App() {
+
+  return (
+    <>
+     <Dice val={7}/>
+    </>
+  )
+}
+
+export default App
---
+++ LUCKY_N/src/Die.jsx
@@ -0,0 +1,6 @@
+import "./Die.css"
+
+export default function Die({v}) {
+    return <div className="Die">{v}</div>;
+}
+
